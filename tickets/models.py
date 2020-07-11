225a35492c5a19cbfc32a010082e5a96316fee5b
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-07-11 20:35:04 +0100

    Ticket model created

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Ticket(models.Model):
+    title = models.CharField('Ticket title', max_length=120)
+    ticket_author = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    TICKET_TYPES = [
+        ('bug', 'Bug'),
+        ('feature', 'Feature')
+    ]
+    TICKET_STATUSES = [
+        ('T', 'To do'),
+        ('D', 'Doing'),
+        ('C', 'Completed')
+    ]
+
+    ticket_type = models.CharField(max_length=10, choices=TICKET_TYPES)
+    ticket_status = models.CharField(max_length=1, choices=TICKET_STATUSES, default='T')
+    created_date = models.DateTimeField('Date created', default=timezone.now)
+
+    def __str__(self):
+        return self.title
