b567973fcb58ba54f3e7356ed0d382e3193c113b
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-07-12 16:30:44 +0100

    Displaying comments in ticket's detailed view

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tickets_list, name='tickets_list'),
-    path('ticket/<int:pk>', views.ticket_detail, name='ticket_detail')
+    path('ticket/<int:pk>', views.ticket_detail, name='ticket_detail'),
 ]
