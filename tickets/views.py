add428271f61239f50d1f8bd165d04b63cb84070
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-07-12 14:03:00 +0100

    Added detailed view for tickets

--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Ticket
 
 
@@ -8,4 +8,11 @@ def tickets_list(request):
         'tickets': tickets
     }
     return render(request, "tickets/tickets_list.html", context)
+
+def ticket_detail(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    context = {
+        'ticket': ticket
+    }
+    return render(request, "tickets/ticket_detail.html", context)
 
