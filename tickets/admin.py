0ce3a44a2fc34e93f6f53778e155664cca2c5ecf
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-07-15 11:49:11 +0100

    Registered vote model

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Ticket, Comment
+from .models import Ticket, Comment, Vote
 
 admin.site.register(Ticket)
 admin.site.register(Comment)
+admin.site.register(Vote)
