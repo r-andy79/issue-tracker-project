7b6d7c50c2518142c6bac94fae74ae0c5daabffa
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-07-29 12:20:11 +0100

    Added profiles application

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tickets.apps.TicketsConfig',
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
