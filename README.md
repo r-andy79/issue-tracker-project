c0515a2a91803e2d379c73a06369b38e9121f224
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-07-29 13:22:16 +0100

    Added profile template to display information about user

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Issue Tracker Application
-## 4th Milestone Project - ... - Code Institute
+## 4th Milestone Project - Full Stack Frameworks with Django - Code Institute
 
 ## UX
 ## User Stories
+### Guest
+* As a guest I want to be able to see the list of tickets that are currently open
+* As a guest I want to be able to see the details of a specific ticket
+
+### Logged User
+
+### Administrator / Developer
+
 ## Features
 ## Technologies Used
 ## Testing
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def profile_view(request):
-    return render(request, "profiles/profile.html")
+def profile_view(request, user_id):
+    user = User.objects.get(id=user_id)
+    print(user)
+    context = {
+        'user': user
+    }
+    return render(request, "profiles/profile.html", context)
