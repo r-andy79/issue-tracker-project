d5d8496c0e8f0cd370f917f7837a85872cfc8421
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-03 16:05:33 +0100

    Linked profile template to main site navigation

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/<int:user_id>', views.profile_view, name='profile_view'),
+    path('profile/<int:user_id>/', views.profile_view, name='profile_view'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404, redir
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from tickets.models import Ticket
+from django.http import HttpResponseForbidden
 
 # Create your views here.
 
 @login_required(login_url='account_login')
 def profile_view(request, user_id):
     user = User.objects.get(id=user_id)
+    if request.user.id == user_id:
+        print('prawda')
+    else:
+        return HttpResponseForbidden()
     username = User.objects.get(username=request.user.username)
     tickets = Ticket.objects.filter(ticket_author=username)
-    print(username)
     context = {
         'user': user,
-        'tickets': tickets
+        'tickets': tickets,
     }
     return render(request, "profiles/profile.html", context)
