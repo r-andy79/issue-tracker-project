bd228b43cdbf9d53ef043417767f9deb0d9debc0
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-17 15:49:56 +0100

    Added draft version of filtering by ticket status

--- tickets/views.py
+++ tickets/views.py
@@ -29,7 +29,10 @@ def tickets_list(request):
 def customize_list(tickets_list, sorting_order):
     order_map = { 
         'descending': '-created_date', 
-        'ascending': 'created_date'
+        'ascending': 'created_date',
+        'to do': 'T',
+        'doing': 'D',
+        'completed': 'C'
         }
     customized_tickets_list = tickets_list.order_by(order_map[sorting_order])
     return customized_tickets_list
@@ -39,6 +42,15 @@ def bugs_list(request):
     doing = Q(ticket_status="D")
     bug = Q(ticket_type="bug")
     bugs = Ticket.objects.filter(bug).filter(to_do | doing)
+    bugs_all = Ticket.objects.filter(bug)
+    q_objects = Q()
+    statuses = request.POST.getlist('status')
+    for s in statuses:
+        q_objects |= Q(ticket_status__contains=s)
+        print(q_objects)
+    
+    b_list = Ticket.objects.filter(bug).filter(q_objects)
+    print(b_list)
     
     sorting_order = request.POST['date'] if request.method == "POST" else 'ascending'
 
