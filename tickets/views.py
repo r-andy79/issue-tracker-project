0172b6cc6cee0f4a2bfcd3cb6532ae907d569897
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-21 17:51:11 +0100

    Refactored bugs and features list functions to use django forms

--- tickets/views.py
+++ tickets/views.py
@@ -9,7 +9,7 @@ from django.db import IntegrityError
 from django.utils import timezone
 from django.core.paginator import Paginator
 from .models import Ticket, Comment, Vote, Payment
-from .forms import TicketForm, CommentForm, PaymentForm
+from .forms import TicketForm, CommentForm, PaymentForm, SearchBugForm, SearchFeatureForm
 from crispy_forms.helper import FormHelper
 from django.http import HttpResponseForbidden, JsonResponse
 import stripe
@@ -35,9 +35,6 @@ def sort_list(tickets_list, sorting_order):
         'vote_ascending': 'total_votes',
         'payments_descending': '-payments_sum',
         'payments_ascending': 'payments_sum',
-        'to do': 'T',
-        'doing': 'D',
-        'completed': 'C'
         }
     sorted_tickets_list = tickets_list.order_by(order_map[sorting_order])
     return sorted_tickets_list
@@ -47,23 +44,102 @@ def filter_list(tickets_list, request):
     statuses = request.POST.getlist('status')
     for status in statuses:
         q_objects |= Q(ticket_status__contains=status)
-        print(q_objects)
     filtered_tickets_list = tickets_list.filter(q_objects)
     return filtered_tickets_list
+
+def make_bugs_query(ticket_type, text=None, sorting_order=None, statuses=None):
+    bugs_query = (
+        Ticket.objects
+        .filter(ticket_type=ticket_type)
+        .annotate(total_votes=Count('vote'))
+    )
+
+    if text:
+        bugs_query = bugs_query.filter(title__icontains=text)
+
+    if sorting_order:
+        bugs_query = tickets_order_by(bugs_query, sorting_order=sorting_order)
     
+    if statuses:
+        bugs_query = ticket_filter_by_statuses(bugs_query, statuses)
+    
+    return bugs_query
+
+        
+def _bugs_list_default(request):
+    bugs = list(make_bugs_query(
+        ticket_type='bug', 
+        sorting_order='vote_descending'
+    ))
+    return render(request, "tickets/bugs_list.html", {
+        'form': SearchBugForm(),
+        'bugs_list': bugs,
+    })
+        
+def _bugs_list_invalid(request, form):
+    bugs = list(make_bugs_query(
+        ticket_type='bug', 
+        sorting_order=form.get_sorting_order()
+    ))
+    return render(request, "tickets/bugs_list.html", {
+        'form': form,
+        'bugs_list': bugs,
+    })
+    
+        
+def _bugs_list_valid(request, form):
+    bugs = list(make_bugs_query(
+        ticket_type='bug',
+        text=form.cleaned_data['text'],
+        sorting_order=form.get_sorting_order(),
+        statuses=form.cleaned_data['ticket_status']
+    ))
+    return render(request, "tickets/bugs_list.html", {
+        'form': form,
+        'bugs_list': bugs,
+    })
+        
+def ticket_filter_by_statuses(query, statuses):
+    q_objects = Q()
+    for status in statuses:
+        q_objects |= Q(ticket_status__contains=status)
+    return query.filter(q_objects)
+  
+
+def tickets_order_by(query, sorting_order):
+    order_map = { 
+        'date_descending': '-created_date', 
+        'date_ascending': 'created_date',
+        'vote_descending': '-total_votes',
+        'vote_ascending': 'total_votes',
+        'payments_sum_descending': '-payments_sum',
+        'payments_sum_ascending': 'payments_sum',
+    }
+    return query.order_by(order_map[sorting_order])
+
 
 def bugs_list(request):
+    if len(request.GET) == 0:
+        return _bugs_list_default(request)
+    else:
+        form = SearchBugForm(request.GET)
+        if form.is_valid():
+            return _bugs_list_valid(request, form)
+        else:
+            return _bugs_list_invalid(request, form)
+
     bug = Q(ticket_type="bug")
     bugs_all = Ticket.objects.filter(bug).annotate(total_votes=Count('vote'))
-    
-    # b_list = Ticket.objects.filter(bug).filter(q_objects)
     
     sorting_order = request.POST['date'] if request.method == "POST" else 'date_ascending'
     ticket_status = request.POST.getlist('status') if request.method == "POST" else ''
     sorted_bugs_list = sort_list(bugs_all, sorting_order)
     filtered_bugs_list = filter_list(sorted_bugs_list, request)
-        
+
+    form = SearchBugForm()
+
     context = {
+        'form': form,
         'bugs_list': filtered_bugs_list,
         'date_ascending_checked': 'checked' if sorting_order == 'date_ascending' else '',
         'date_descending_checked': 'checked' if sorting_order == 'date_descending' else '',
@@ -75,7 +151,67 @@ def bugs_list(request):
     }
     return render(request, "tickets/bugs_list.html", context)
 
+def make_features_query(ticket_type, text=None, sorting_order=None, statuses=None):
+    features_query = (
+        Ticket.objects
+        .filter(ticket_type=ticket_type)
+        .annotate(payments_sum=Sum('payment__payment_value'))
+    )
+
+    if text:
+        features_query = features_query.filter(title__icontains=text)
+
+    if sorting_order:
+        features_query = tickets_order_by(features_query, sorting_order=sorting_order)
+    
+    if statuses:
+        features_query = ticket_filter_by_statuses(features_query, statuses)
+    
+    return features_query
+
+def _features_list_default(request):
+    features = list(make_features_query(
+        ticket_type='feature', 
+        sorting_order='payments_sum_descending'
+    ))
+    return render(request, "tickets/features_list.html", {
+        'form': SearchFeatureForm(),
+        'features_list': features,
+    })
+        
+def _features_list_invalid(request, form):
+    featuress = list(make_features_query(
+        ticket_type='feature', 
+        sorting_order=form.get_sorting_order()
+    ))
+    return render(request, "tickets/features_list.html", {
+        'form': form,
+        'features_list': features,
+    })
+    
+        
+def _features_list_valid(request, form):
+    features = list(make_features_query(
+        ticket_type='feature',
+        text=form.cleaned_data['text'],
+        sorting_order=form.get_sorting_order(),
+        statuses=form.cleaned_data['ticket_status']
+    ))
+    return render(request, "tickets/features_list.html", {
+        'form': form,
+        'features_list': features,
+    })
+
 def features_list(request):
+    if len(request.GET) == 0:
+        return _features_list_default(request)
+    else:
+        form = SearchFeatureForm(request.GET)
+        if form.is_valid():
+            return _features_list_valid(request, form)
+        else:
+            return _features_list_invalid(request, form)
+
     feature = Q(ticket_type="feature")
     features_all = Ticket.objects.filter(feature).annotate(payments_sum=Sum('payment__payment_value'))
     
@@ -86,6 +222,7 @@ def features_list(request):
     filtered_features_list = filter_list(sorted_features_list, request)
         
     context = {
+        'form': form,
         'features_list': filtered_features_list,
         'date_ascending_checked': 'checked' if sorting_order == 'date_ascending' else '',
         'date_descending_checked': 'checked' if sorting_order == 'date_descending' else '',
@@ -193,12 +330,8 @@ def ticket_vote(request, ticket_id, user_id):
 def pay(request, pk):
     ticket = get_object_or_404(Ticket, pk=pk)
     if request.method == "POST":
-        print('method POST')
         form = PaymentForm(request.POST)
-        if form.is_valid(): # akcja is_valid notuje wewnątrz formularza błędy
-            # 2. przypadek gdy wszystkie dane są w porządku
-            print('form valid')
-            # tu tworzę dwa obiekty jakie są wymagane z poziomu stripe
+        if form.is_valid():
             form.instance.user = request.user
             form.instance.ticket = ticket
             customer = stripe.Customer.create(
@@ -213,7 +346,6 @@ def pay(request, pk):
                 description = 'Donation',
                 #idempotency_key=
             )
-            print(charge)
             form.save()
             return redirect(reverse('success', args=[form.instance.payment_value]))
     else:
@@ -227,9 +359,6 @@ def pay(request, pk):
     return render(request, "tickets/payment.html", context) # redirect do success
 
 def charge(request):
-    amount = 5
-    if request.method == 'POST':
-        print('Data', request.POST)
     return redirect(reverse('success', args=[amount]))
 
 @csrf_exempt
@@ -240,4 +369,5 @@ def stripe_config(request):
 
 def successMsg(request, args):
     amount = args
+    messages.success(request, f'Thank you for your payment of %a euro' %amount)
     return render(request, 'tickets/success.html', {'amount': amount})
