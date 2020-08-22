bdd11a6f8ebffddd96491c2575e78ee40e22c225
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-22 16:54:48 +0100

    App added to the settings file

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'tickets.apps.TicketsConfig',
     'profiles.apps.ProfilesConfig',
+    'tickets_management.apps.TicketsManagementConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
