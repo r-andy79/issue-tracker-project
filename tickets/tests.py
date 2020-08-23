9247026e7ac85fadcb7f3562b7391668ec41d936
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-23 21:23:24 +0100

    Allauth templates moved to a different folder

--- tickets/tests.py
+++ tickets/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from tickets.models import Ticket, Comment, Vote, Payment
 # Create your tests here.
+
+class TicketTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_bug_created(self):
+        pass
