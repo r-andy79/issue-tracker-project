394550d82cd4b1a934d90454bdcdac7d67bfe322
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-27 19:43:39 +0100

    Removed querysets

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.db.models import Q, Sum, Count
 from tickets.models import Ticket, Vote, Payment, Comment
-from django.http import HttpResponseForbidden
 
 # Create your views here.
 
@@ -11,11 +9,9 @@ from django.http import HttpResponseForbidden
 def profile_view(request):
 
     tickets = Ticket.objects.filter(ticket_author=request.user)
-    votes = Vote.objects.filter(user=request.user)
 
     context = {
         'user': request.user,
         'tickets': tickets,
-        'votes': votes,
     }
     return render(request, "profiles/profile.html", context)
