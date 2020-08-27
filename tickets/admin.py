178daf0100fde63b8a117dc10378dafde9be6ec8
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-27 19:31:42 +0100

    Added sending emails on ticket status change

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from django.db.models import Sum, Count
 from .models import Ticket, Comment, Vote, Payment, BugTicket, FeatureTicket
+from django.core.mail import send_mail
+from django.conf import settings
+from smtplib import SMTPException
+
+class TicketAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        old_obj = Ticket.objects.get(id=obj.id)
+        if old_obj.ticket_status != obj.ticket_status:
+            try:
+                send_mail(
+                    f'{obj.title} - change of status',
+                    f'Please be advised that the status of your ticket has changed to {obj.get_ticket_status_display()}',
+                    settings.DEFAULT_FROM_EMAIL,
+                    [obj.ticket_author.email],
+                    fail_silently=False,
+                )
+            except SMTPException:
+                # TODO zmień print na logging
+                print('There is something wrong with the email')
+            super().save_model(request, obj, form, change)
 
 @admin.register(BugTicket)
-class TicketBugAdmin(admin.ModelAdmin):
+class TicketBugAdmin(TicketAdmin):
     list_display=['title', 'total_votes', 'ticket_author', 'ticket_status', 'created_date']
 
     def get_queryset(self, request):
@@ -13,7 +33,7 @@ class TicketBugAdmin(admin.ModelAdmin):
         return ticket.total_votes
 
 @admin.register(FeatureTicket)
-class TicketFeatureAdmin(admin.ModelAdmin):
+class TicketFeatureAdmin(TicketAdmin):
     list_display=['title', 'payments_sum', 'ticket_author', 'ticket_status', 'created_date']
     
     def get_queryset(self, request):
