619ef1527458d5202f2d076c6ecbf7b6b0cc8049
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-08-30 13:44:06 +0100

    Unused app removed

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -42,7 +42,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'tickets.apps.TicketsConfig',
     'profiles.apps.ProfilesConfig',
-    'tickets_management.apps.TicketsManagementConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
